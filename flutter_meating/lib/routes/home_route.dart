5286e3b70c9bb606d30504fae76380cf09b996ff
albi_frattini <alberto.frattini@mail.polimi.it>
2019-02-25 11:25:28 +0100

    Added BottomNavigationBar and ready to create Chat

--- flutter_meating/lib/routes/home_route.dart
+++ flutter_meating/lib/routes/home_route.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_meating/utils/authentication.dart';
+import 'explore_route.dart';
+import 'chat_route.dart';
+import 'profile_route.dart';
 
 class HomeRoute extends StatefulWidget {
 
@@ -17,10 +20,45 @@ class HomeRoute extends StatefulWidget {
 
 class _HomeRouteState extends State<HomeRoute> {
 
+  int _currentIndex = 0;
+  final List<Widget> _routes = [
+    ExploreRoute(),
+    ChatRoute(),
+    ProfileRoute()
+  ];
+
+  Widget _bottomNavigationBar() {
+    return BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTabTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text("Home"),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.mail),
+            title: new Text("Inbox"),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.person),
+            title: new Text("Profile"),
+          ),
+        ]
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      bottomNavigationBar: _bottomNavigationBar(),
+      body: _routes[_currentIndex],
       floatingActionButton: new RaisedButton(
         child: Text("Sign Out"),
           onPressed: () {
