76ab3c7e99b71b908f7c39936709338e8637be1f
albi_frattini <alberto.frattini@mail.polimi.it>
2019-02-26 17:37:10 +0100

    Inbox Route added

---
+++ flutter_meating/lib/routes/inbox_route.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'chat_route.dart';
+
+class InboxRoute extends StatefulWidget {
+
+  final String userId;
+
+  InboxRoute({this.userId});
+
+  @override
+  State<StatefulWidget> createState() => _InboxRouteState(currentUserId: userId);
+
+}
+
+class _InboxRouteState extends State<InboxRoute> {
+
+  final String currentUserId;
+
+  _InboxRouteState({this.currentUserId});
+
+  Widget _buildListChats() {
+    return Container(
+      child: StreamBuilder(
+        stream: Firestore.instance.collection('users').snapshots(),
+          builder: (context, snapshot) {
+            if(!snapshot.hasData) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return ListView.builder(
+                  itemBuilder: (context, index) => buildItem(index, snapshot.data.documents[index]),
+                  itemCount: snapshot.data.documents.length,
+              );
+            }
+          }
+      ),
+    );
+  }
+
+  Widget buildItem(int index, DocumentSnapshot document) {
+    if(document['id'] == currentUserId) {
+      return Container();
+    } else {
+      return Container(
+        child: FlatButton(
+            child: Row(
+              children: <Widget>[
+                Flexible(
+                    child: Container(
+                      child: Text(
+                        "id: ${document['id']}",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      alignment: Alignment.centerLeft,
+                      margin: EdgeInsets.fromLTRB(10.0, 0.0, 0.0, 5.0),
+                    ),
+                ),
+              ],
+            ),
+          onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ChatRoute(userId: currentUserId,recipientId: document.documentID))
+              );
+          },
+          color: Colors.transparent,
+          padding: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0.0)),
+        ),
+        // margin: EdgeInsets.only(top: 5.0),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text("Inbox"),
+        elevation: 0.0,
+      ),
+      body: _buildListChats(),
+    );
+  }
+
+}
+
