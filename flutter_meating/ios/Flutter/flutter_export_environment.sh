705f63d921c319b3cece3d2ef73ccbef6cba7052
albi_frattini <alberto.frattini@mail.polimi.it>
2019-08-25 18:02:34 +0200

    Android emulator

--- flutter_meating/ios/Flutter/flutter_export_environment.sh
+++ flutter_meating/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/alberto/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/alberto/development/gabri/meating/flutter_meating"
-export "FLUTTER_TARGET=/Users/alberto/development/gabri/meating/flutter_meating/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/alberto/development/meating/flutter_meating"
+export "FLUTTER_TARGET=/Users/alberto/development/meating/flutter_meating/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/alberto/development/flutter/bin/cache/artifacts/engine/ios"
